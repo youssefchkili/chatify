5bcab93d5bd2bd85487e22a93ecf4dabc297beda
Youssef <youssefchkili04@gmail.com>
2026-02-05 00:30:39 +0100

    inital commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
