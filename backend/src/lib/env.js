8342fbec9b49903d0be8b5ef410d592e9c423d8e
Youssef <youssefchkili04@gmail.com>
2026-02-12 18:34:40 +0100

    update profile endpoint added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -8,7 +8,10 @@ export const ENV = {
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     NODE_ENV: process.env.NODE_ENV,
     EMAIL_FROM: process.env.EMAIL_FROM,
-    EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME
+    EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
 
 }
 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express";
-import {signup, login ,logout } from "../controllers/auth.controller.js";
+import {signup, login ,logout , updateProfile } from "../controllers/auth.controller.js";
+import {protectRoute} from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 
 
@@ -7,6 +9,12 @@ router.post('/signup', signup);
 
 router.post('/login', login );
 
+
 router.post('/logout', logout);
 
+router.put('/update-profile', protectRoute, updateProfile);
+
+router.get('/check', protectRoute, (req,res) => res.status(200).json(req.user));
+;
 export default router;
+
