5351a529d044553dc04e77cb5a32054663895708
Youssef <youssefchkili04@gmail.com>
2026-02-12 20:12:28 +0100

    message routes created

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,19 @@
 import express from  "express";
+import { getAllContacts , getChatPartners,getMessagesByUserId , sendMessage} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get('/send', (req,res) =>{
-    res.send('Send message endpoint');
-});
+router.use(arcjetProtection,protectRoute);
+
+
+router.get("/contacts" ,getAllContacts);
+
+router.get("/chats",getChatPartners);
+
+router.get("/:id", getMessagesByUserId);
+
+router.post("/send/:id",sendMessage);
 
 export default router;
